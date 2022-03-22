b3f43a2358051df3fe5d00303b727a7b4664a814
anotherfaizan <mdfaizankhan2002@gmail.com>
2022-03-22 23:13:43 +0530

    added screenshots

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -1,8 +1,8 @@
 import 'package:bmi_calculator/components/bottom_button.dart';
 import 'package:bmi_calculator/components/constants.dart';
 import 'package:bmi_calculator/components/reusable_card.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class ResultPage extends StatelessWidget {
 
@@ -33,29 +33,45 @@ class ResultPage extends StatelessWidget {
             ),
           ),
           Expanded(
-            flex: 5,
-              child: ReusableCard(
-                  colour: kActiveCardColor,
-                cardChild: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      resultText.toUpperCase(),
-                      style: kResultTextStyle,
-                    ),
-                    Text(
-                      bmiResult,
-                      style: kBMITextStyle,
-                    ),
-                    Text(
-                      interpretation,
-                      textAlign: TextAlign.center,
-                      style: kBodyTextStyle,
-                    ),
-                  ],
-                ),
-              ),),
+            flex: 6,
+            child: SingleChildScrollView(
+                child: ReusableCard(
+                    colour: kActiveCardColor,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SfRadialGauge(axes: <RadialAxis>[
+                        RadialAxis(minimum: 0, maximum: 60, ranges: <GaugeRange>[
+                          GaugeRange(startValue: 0, endValue: 20, color: Colors.green),
+                          GaugeRange(startValue: 20, endValue: 40, color: Colors.orange),
+                          GaugeRange(startValue: 40, endValue: 60, color: Colors.red)
+                        ], pointers: <GaugePointer>[
+                          NeedlePointer(value: double.parse(bmiResult))
+                        ], annotations: <GaugeAnnotation>[
+                          GaugeAnnotation(
+                              widget: Container(
+                                  child: Text('${bmiResult}',
+                                      style: const TextStyle(
+                                          color: Color(0xFF24D876),
+                                          fontSize: 25, fontWeight: FontWeight.bold))),
+                              angle: 90,
+                              positionFactor: 0.5)
+                        ])
+                      ]),
+                      Text(
+                        resultText.toUpperCase(),
+                        style: kResultTextStyle,
+                      ),
+                      Text(
+                        interpretation,
+                        textAlign: TextAlign.center,
+                        style: kBodyTextStyle,
+                      ),
+                    ],
+                  ),
+                ),),
+          ),
           BottomButton(
             buttonTitle: 'RE-CALCULATE',
             onTap: (){
